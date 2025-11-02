2e52f5ad3906e5f140148c73d1a4f61b71671bf9
Fan wansong <fwscool@163.com>
2025-11-02 20:04:05 +0800

    【UI】main_title加入K键彩蛋

--- Scripts/UI/main_title.gd
+++ Scripts/UI/main_title.gd
@@ -1,5 +1,7 @@
 extends Control
 
+# 获取AnimationPlayer节点的引用
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +11,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+# 处理输入事件
+func _input(event: InputEvent) -> void:
+	# 检测是否按下K键
+	if event is InputEventKey and event.pressed and not event.echo:
+		if event.keycode == KEY_K:
+			# 播放kkey动画
+			if animation_player:
+				animation_player.play("kkey")
 
 
 func _on_btn_single_play_pressed() -> void:
