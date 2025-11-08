99c2653193e376e2f1905d7a0542456b96e236d5
a$ong <fwscool@163.com>
2025-11-09 01:40:27 +0800

    【UI】加入cutscene_0,加入skip以及调整跳转

--- Scripts/UI/cutscene_1.gd
+++ Scripts/UI/cutscene_1.gd
@@ -16,3 +16,7 @@ func _process(delta: float) -> void:
 func _on_btn_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/UI/start_menu.tscn")
 	pass # Replace with function body.
+
+
+func _on_skip_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/start_menu.tscn")
