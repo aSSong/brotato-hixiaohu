27c838bbfc19a288e1b7aa00687896272de4510c
a$ong <fwscool@163.com>
2025-11-20 23:15:29 +0800

    【UI】标题界面显示角色名字，如果有存档则跳过cutscene

--- Scripts/UI/main_title.gd
+++ Scripts/UI/main_title.gd
@@ -2,12 +2,24 @@ extends Control
 
 # 获取AnimationPlayer节点的引用
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var player_info: Label = $menu/VBoxContainer/player_info
+
+	# 检查存档中的名字
+var player_name = SaveManager.get_player_name()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# 播放标题BGM
 	BGMManager.play_bgm("title")
 	print("[MainTitle] 开始播放标题BGM")
+	
+
+	if player_name != "":
+		var floor_name = SaveManager.get_floor_name()
+		player_info.text = player_name + "  " + floor_name
+		animation_player.play("kkey")
+	else:
+		player_info.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,8 +40,11 @@ func _input(event: InputEvent) -> void:
 func _on_btn_single_play_pressed() -> void:
 	# 设置为survival模式（默认模式）
 	GameMain.current_mode_id = "survival"
-	get_tree().change_scene_to_file("res://scenes/UI/cutscene_0.tscn")
-	pass # Replace with function body.
+	
+	if player_name != "":
+		get_tree().change_scene_to_file("res://scenes/UI/start_menu.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scenes/UI/cutscene_0.tscn")
 
 
 func _on_btn_multi_play_pressed() -> void:
