ede58b9fb9efa35d991e93e97ef198a947d36a3a
a$ong <fwscool@163.com>
2025-12-10 23:59:58 +0800

    【新界面】设置界面框架

---
+++ Scripts/UI/settings_ui.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _on_cut_1_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/cutscene_playback_1.tscn")
+
+
+func _on_cut_2_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/cutscene_playback_2.tscn")
+
+
+func _on_stuffbtn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/cutscene_playback_1.tscn")
+
+
+func _on_filesbtn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/cutscene_playback_1.tscn")
+
+
+func _on_backbtn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/main_title.tscn")
